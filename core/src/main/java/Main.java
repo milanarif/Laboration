6d443e687867df9bc8ea7e0cba4b94a4d7834271
Shorl <milanarif@gmail.com>
2021-05-28 10:51:00 +0200

    Initial commit

---
+++ core/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(80);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
